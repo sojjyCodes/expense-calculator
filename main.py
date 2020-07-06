afa62692efeab0aa566a049d1e04bbe98685e354
Ebenezer <66065626+sojjyCodes@users.noreply.github.com>
2020-07-06 11:03:14 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+print("How much did you spend on the 1st day? ")
+first_day = int(input('Enter amount: '))
+
+print('How much did you spend on the 2nd day? ')
+second_day = float(input('Enter amount: '))
+
+print('How much did you spend on the 3rd day? ')
+third_day = float(input('Enter amount: '))
+
+print('How much did you spend on the 4th day? ')
+forth_day = float(input('Enter amount: '))
+
+print('How much did you spend on the 5th day? ')
+fifth_day = float(input('Enter amount: '))
+
+print('How much did you spend on the 6th day? ')
+sixth_day = float(input('Enter amount: '))
+
+print('How much did you spend on the 7th day? ')
+seventh_day = float(input('Enter amount: '))
+
+ans = (first_day + second_day + third_day + forth_day + fifth_day + sixth_day + seventh_day)
+print(ans)
+
+
